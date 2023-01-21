3bf98b0fba3eba3de2363d6b95b7a419e8953a99
DANIEL PAT <DANIEL_PAT1888@hotmail.com>
2023-01-21 12:17:18 -0600

    resultado2 es la buena

---
+++ main.py
@@ -0,0 +1,44 @@
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+img = cv.imread('RX.png',0)
+img = cv.medianBlur(img,5)
+ret,th1 = cv.threshold(img,70,255,cv.THRESH_BINARY)
+th2 = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_MEAN_C,\
+            cv.THRESH_BINARY,11,2)
+th3 = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,\
+            cv.THRESH_BINARY,11,2)
+titles = ['Original Image', 'Global Thresholding (v = 127)',
+            'Adaptive Mean Thresholding', 'Adaptive Gaussian Thresholding']
+images = [img, th1, th2, th3]
+for i in range(4):
+    plt.subplot(2,2,i+1),plt.imshow(images[i],'gray')
+    plt.title(titles[i])
+    plt.xticks([]),plt.yticks([])
+    result = cv.subtract(th3, th1)
+    result2 = cv.subtract(img, result)
+    cv.imshow('Result', result)
+    cv.imshow('result2', result2)
+plt.show()
+"""ret,thresh1 = cv.threshold(img,127,255,cv.THRESH_BINARY)
+ret,thresh2 = cv.threshold(img,127,255,cv.THRESH_BINARY_INV)
+ret,thresh3 = cv.threshold(img,127,255,cv.THRESH_TRUNC)
+ret,thresh4 = cv.threshold(img,127,255,cv.THRESH_TOZERO)
+ret,thresh5 = cv.threshold(img,127,255,cv.THRESH_TOZERO_INV)
+titles = ['Original Image','BINARY','BINARY_INV','TRUNC','TOZERO','TOZERO_INV']
+images = [img, thresh1, thresh2, thresh3, thresh4, thresh5]
+for i in range(6):
+    plt.subplot(2,3,i+1),plt.imshow(images[i],'gray',vmin=0,vmax=255)
+    plt.title(titles[i])
+    plt.xticks([]),plt.yticks([])
+    laplacian = cv.Laplacian(img, cv.CV_64F)
+    sobelx = cv.Sobel(img, cv.CV_64F, 1, 0, ksize=5)
+    result = cv.subtract(thresh2, thresh3)
+    result2 = cv.subtract(img, result)
+    com = sobelx.astype(img.dtype)
+    xmb = cv.subtract(com, result)
+    cv.imshow('Result', result)
+    cv.imshow('sobelx', sobelx)
+    cv.imshow('result2', result2)
+plt.show()"""
+
